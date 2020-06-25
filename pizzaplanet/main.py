a10bdddcf91bd87b2b3b6abf8b4aa54771045f1f
Your name <telodato.16@est.ucab.edu.ve>
2020-06-25 07:36:26 -0400

    Añadido menu de combo al menu principal

--- pizzaplanet/main.py
+++ pizzaplanet/main.py
@@ -1,4 +1,5 @@
 from pizzaplanet import estadisticas
+from pizzaplanet import combo
 
 class Main:
     def menu(self):
@@ -8,20 +9,30 @@ class Main:
             print("1)Registrar pedido.")
             print("2)Realizar un reporte")
             print("3)Cargar Archivos")
+            print("4)Ver Combo del dia")
             print("0)Exit")
 
             option = self.Validate_Entry()
 
             if(option == 1):
                 break
+
             elif(option == 2):
                 self.cls(25)
                 e = estadisticas.Estadisticas()
                 e.main()
+
             elif(option == 3):
                 break
+
+            elif(option == 4):
+                self.cls(25)
+                c = combo.Combo()
+                c.main()
+
             elif(option == 0):
                 break
+
         input("Gracias por su atencion hasta luego")
 
 
@@ -29,7 +40,7 @@ class Main:
         entry = input("Por favor ingrese la opcion: ")
         try:
             entry = int(entry)
-            if (entry <= 0) or (entry > 3):
+            if (entry < 0) or (entry > 4):
                 self.cls(25)
                 print("Opcion invalida!! Ingrese una opcion valida")
             else:
